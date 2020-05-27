1fe4d94fcb3de30ca6c3fb47d54ab6d0ae422b1c
kartikey tyagi <kartikey110813@gmail.com>
2020-05-27 17:01:03 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,14 @@
+function myfunction(){
+    alert("This Service is not available for now . You will be notified once this service will be available. Thank You and sorry for inconvinience");
+
+}
+
+function yourfunction(){
+    alert("This Service is not available for now . You will be notified once this service will be available. Thank You and sorry for inconvinience");
+
+}
+
+function ourfunction() {
+    alert("This Service is not available for now . You will be notified once this service will be available. Thank You and sorry for inconvinience");
+
+}
