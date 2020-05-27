ea5d6d7231eea0b13ba969de55c9ef4a878d478d
dowhile03 <65396487+dowhile03@users.noreply.github.com>
2020-05-27 17:15:19 +0530

    commiting about u
    commiting about us

---
+++ aboutus.js
@@ -0,0 +1,10 @@
+
+function myfunction() {
+    window.location.replace('about.html')
+}
+function myfunction1() {
+    window.location.replace('mission.html')
+}
+function myfunction2() {
+    window.location.replace('vision.html')
+}
